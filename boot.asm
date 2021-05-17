bb1d34f9908a99b6f6c849b60222eb67551e2250
ZeroInverse <ujjwel17179@cse.ssn.edu.in>
2021-05-17 16:17:18 +0530

    Hello World v2

--- boot.asm
+++ boot.asm
@@ -2,15 +2,26 @@ org 0x7c00
 bits 16
 
 start:
-    mov ah, 0eh
-    mov al, 'U'
-    mov bx, 0
-    int 0x10    ; Int 10/AH=0Eh - VIDEO - TELETYPE OUTPUT
-    jmp $
+    mov si, string
+    call print
 
-times 510-($-$$) db 0
-; 510 - (current address - section starting address) times DB 0
-dw 0xaa55
-; DB - 1 W
-; DW - 2 W
-; DD - 4 W
+print:
+    mov bx, 0; used for forground and background color
+.loop:
+    lodsb; Load SI ot AL and increment SI
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+string: db 'Hello World!', 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55; bootloader signature is 0x55aa, written in little endian because Intel
