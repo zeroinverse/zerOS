fd02071b62294462269b0b73a02220e0a885169f
ZeroInverse <ujjwel17179@cse.ssn.edu.in>
2021-05-19 09:38:57 +0530

    Add Interrupt Vector Table

--- boot.asm
+++ boot.asm
@@ -10,6 +10,21 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
+; https://wiki.osdev.org/Exceptions
+handle_zero:
+    mov ah, 0eh
+    mov al, '0'
+    mov bx, 0x00
+    int 0x10 ; print Z in case this routine is called
+    iret
+
+handle_one:
+    mov ah, 0eh
+    mov al, '1'
+    mov bx, 0x00
+    int 0x10
+    iret
+
 step2:
     cli ; clear interrupts
     mov ax, 0x7c0
@@ -20,14 +35,25 @@ step2:
     mov sp, 0x7c00
     sti ; enable interrupts
     
+    ; provide custom routine for INT 0 exception
+    mov word[ss:0x00], handle_zero
+    mov word[ss:0x02], 0x7c0
+    mov ax, 0x00
+    div ax ; divide by zero calls INT 0 exception
+
+    mov word[ss:0x04], handle_one
+    mov word[ss:0x06], 0x7c0
+    ; call exception 
+    int 1
+    
     mov si, string
     call print
     jmp $
 
 print:
-    mov bx, 0; used for forground and background color
+    mov bx, 0 ; used for forground and background color
 .loop:
-    lodsb; Load SI ot AL and increment SI
+    lodsb ; Load SI ot AL and increment SI
     cmp al, 0
     je .done
     call print_char
