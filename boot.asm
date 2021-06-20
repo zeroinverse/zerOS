495667a3e4748dd8a1cfb30efbe3a5b9bd73bd87
zeroinverse <ujjwel17179@cse.ssn.edu.in>
2021-06-20 14:10:18 +0530

    Enter protected mode

--- boot.asm
+++ boot.asm
@@ -1,62 +1,77 @@
-org 0
-bits 16
-; https://wiki.osdev.org/FAT#BPB_.28BIOS_Parameter_Block.29
+ORG 0x7c00
+BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
     jmp short start
     nop
 
-times 33 db 0
-
+ times 33 db 0
+ 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
-    cli ; clear interrupts
-    mov ax, 0x7c0
+    cli ; Clear Interrupts
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
-    sti ; enable interrupts
-    ; read data from hard disk using Cylinder-head-sector
-    mov ah, 2 ; read sector command
-    mov al, 1 ; read one sector
-    mov ch, 0 ; low eight bits of cylinder number
-    mov cl, 2 ; read sector 2
-    mov dh, 0 ; head number
-    ; drive number is set by default (bit 7 set for hard disk)
-    mov bx, buffer ; write read data to buffer
-    int 0x13
-    jc error
-    mov si, buffer
-    call print
+    sti ; Enables Interrupts
+
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+ 
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
 
-error:
-    mov si, err_message
-    call print_char
-    jmp $
 
-print:
-    mov bx, 0 ; used for forground and background color
-.loop:
-    lodsb ; Load SI ot AL and increment SI
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-err_message: db 'failed to load sector', 0
-
-times 510 - ($ - $$) db 0
-dw 0xAA55; bootloader signature is 0x55aa, written in little endian because Intel 
-; buffer points to after first sector
-buffer; dummmy variable so that the disk read does not override anything
+times 510-($ - $$) db 0
+dw 0xAA55
